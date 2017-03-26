a7bc6762a4837a48694f5eab275407827f043a80
Suwashimizu <suwawa2013@gmail.com>
2017-03-26 22:45:50 +0900

    add api call

--- src/components/PageOne.js
+++ src/components/PageOne.js
@@ -15,19 +15,42 @@ export default class PageOne extends Component {
         <TextInput 
           style={{height: 48}}
           placeholder="saerch book name."
-          onChangeText={(text) => this.setState({text})}
+          onChangeText={(text) => {
+              if(text!=null){
+                fetchData(text);
+              }
+              this.setState({text});
+            }
+          }
         />
         <Text onPress={Actions.pageTwo}>This is PageOne!!!!</Text>
         <Text style={{padding: 10, fontSize: 42}}>
           {this.state.text}
         </Text>
-         <Text style={{padding: 10, fontSize: 42}}>
+        <Text style={{padding: 10, fontSize: 42}}>
           
         </Text>
       </View>
     )
   }
 }
+
+
+const GOOGLE_BOOK_ENTRY_URL = "https://www.googleapis.com/books/v1/volumes?q=intitle:%E3%82%B9%E3%83%AC%E3%82%A4%E3%83%A4%E3%83%BC%E3%82%BA+inauthor:%E5%A4%A7%E5%A1%9A";
+
+const fetchData = (text,app) => {
+    console.log(text);
+
+    fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:" + text)
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log(responseData.items);
+      app.setState({
+        dataSource: app.state.dataSource.cloneWithRows(responseData.items)
+      });
+    })
+    .done();
+  }
 
 const styles = StyleSheet.create({
   container: {
