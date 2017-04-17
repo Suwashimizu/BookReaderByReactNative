2fc4e10746c566887633adeebedb302b1eccd279
Suwashimizu <suwawa2013@gmail.com>
2017-04-18 00:10:18 +0900

    add Components/SeatchEmptyItem

--- src/native/containers/PageOne.js
+++ src/native/containers/PageOne.js
@@ -1,10 +1,10 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { 
+import {
   View,
   Image,
-  Text, 
+  Text,
   TextInput,
   StyleSheet,
   ListView,
@@ -16,14 +16,15 @@ import ActionTypes from '../../actions/ActionTypes';
 import BookEntry from '../../store/BookEntry';
 import Icon from 'react-native-vector-icons/Ionicons';
 import TextInputLayout from '../components/TextInputLayout';
+import SeachResultEmptyItem from '../components/SearchResultEmptyItem';
 
 export default class PageOne extends Component {
 
   constructor() {
     super();
-    this.state = {text: ''};
+    this.state = { text: '' };
 
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
       //Dataの設定
       dataSource: ds.cloneWithRows([])
@@ -32,13 +33,13 @@ export default class PageOne extends Component {
   }
 
   //ListItemのRender
-  renderEntry(entry){
-  console.log('callRenderEntry');
+  renderEntry(entry) {
+    console.log('callRenderEntry');
     return (
-      <TouchableNativeFeedback onPress={() => (Actions.pageTwo({book:entry}))}>
+      <TouchableNativeFeedback onPress={() => (Actions.pageTwo({ book: entry }))}>
         <View>
           <View style={styles.listItem}>
-            <Thumbnail imageLinks = {entry.volumeInfo.imageLinks} />
+            <Thumbnail imageLinks={entry.volumeInfo.imageLinks} />
 
             <View style={styles.rightContainer}>
               <Text style={styles.title} >
@@ -46,7 +47,7 @@ export default class PageOne extends Component {
               </Text>
             </View>
           </View>
-          <View style={styles.separator}/>
+          <View style={styles.separator} />
         </View>
       </TouchableNativeFeedback>
     );
@@ -56,26 +57,27 @@ export default class PageOne extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.inputBackground}>
-          <TextInputLayout 
-            onChangeText = {(text) => {
-              if(text != null) fetchData(text,this);
-              
-              this.setState({text});
+          <TextInputLayout
+            onChangeText={(text) => {
+              if (text != null) fetchData(text, this);
+
+              this.setState({ text });
             }
-          }
+            }
           />
         </View>
 
-        { this.validEntry() ?
+        {this.validEntry() ?
           <ListView
             style={styles.listView}
             dataSource={this.state.dataSource}
-            renderRow={this.renderEntry}/> : null}
+            renderRow={this.renderEntry}
+          /> : <SeachResultEmptyItem />}
       </View>
     )
   }
 
-  validEntry(){
+  validEntry() {
     return this.state.dataSource && this.state.text
   }
 }
@@ -85,17 +87,17 @@ const Body = () => (
   <Text >Posts</Text>
 );
 
-const Thumbnail = ({imageLinks}) => {
+const Thumbnail = ({ imageLinks }) => {
 
-  if(imageLinks){
-    return(
-      <Image 
-        source={{uri: imageLinks.smallThumbnail}}
-        style={styles.thumbnail}/>
+  if (imageLinks) {
+    return (
+      <Image
+        source={{ uri: imageLinks.smallThumbnail }}
+        style={styles.thumbnail} />
     )
   } else {
-    return(
-      <Text style={[styles.thumbnail,styles.noImage]}>NoImage</Text>
+    return (
+      <Text style={[styles.thumbnail, styles.noImage]}>NoImage</Text>
     )
   }
 }
@@ -103,19 +105,19 @@ const Thumbnail = ({imageLinks}) => {
 const GOOGLE_BOOK_ENTRY_URL = "https://www.googleapis.com/books/v1/volumes?q=intitle:%E3%82%B9%E3%83%AC%E3%82%A4%E3%83%A4%E3%83%BC%E3%82%BA+inauthor:%E5%A4%A7%E5%A1%9A";
 
 //Fetcherは外に出す
-const fetchData = (text,app) => {
-    console.log(text);
+const fetchData = (text, app) => {
+  console.log(text);
 
-    fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:" + text)
+  fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:" + text)
     .then((response) => response.json())
     .then((responseData) => {
-      if(responseData.items){
+      if (responseData.items) {
         app.setState({
           dataSource: app.state.dataSource.cloneWithRows(responseData.items)
         });
       }
-    }).done(); 
-  }
+    }).done();
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -169,42 +171,42 @@ const styles = StyleSheet.create({
 });
 
 const _TEST_ENTRY_DATA = {
-    kind: "books#volumes",
-    totalItems: 4,
-    items: [
+  kind: "books#volumes",
+  totalItems: 4,
+  items: [
     {
-     kind: "books#volume",
-     id: "n0WU-RX8-yoC",
-     etag: "i1EbgqzMFpU",
-     selfLink: "https://www.googleapis.com/books/v1/volumes/n0WU-RX8-yoC",
-     volumeInfo: {
-      title: "スレイヤーズ　水竜王の騎士(6)",
-      authors: [
-      "トミイ　大塚"
-      ],
-      imageLinks: {
-       smallThumbnail: "http://books.google.com/books/content?id=n0WU-RX8-yoC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-       thumbnail: "http://books.google.com/books/content?id=n0WU-RX8-yoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+      kind: "books#volume",
+      id: "n0WU-RX8-yoC",
+      etag: "i1EbgqzMFpU",
+      selfLink: "https://www.googleapis.com/books/v1/volumes/n0WU-RX8-yoC",
+      volumeInfo: {
+        title: "スレイヤーズ　水竜王の騎士(6)",
+        authors: [
+          "トミイ　大塚"
+        ],
+        imageLinks: {
+          smallThumbnail: "http://books.google.com/books/content?id=n0WU-RX8-yoC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+          thumbnail: "http://books.google.com/books/content?id=n0WU-RX8-yoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        }
       }
-     }
     },
     {
-     kind: "books#volume",
-     id: "n0WU-RX8-yoC",
-     etag: "i1EbgqzMFpU",
-     selfLink: "https://www.googleapis.com/books/v1/volumes/n0WU-RX8-yoC",
-     volumeInfo: {
-      title: "スレイヤーズ　水竜王の騎士(1)",
-      authors: [
-      "トミイ　大塚"
-      ],
-      imageLinks: {
-       smallThumbnail: "http://books.google.com/books/content?id=n0WU-RX8-yoC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-       thumbnail: "http://books.google.com/books/content?id=n0WU-RX8-yoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+      kind: "books#volume",
+      id: "n0WU-RX8-yoC",
+      etag: "i1EbgqzMFpU",
+      selfLink: "https://www.googleapis.com/books/v1/volumes/n0WU-RX8-yoC",
+      volumeInfo: {
+        title: "スレイヤーズ　水竜王の騎士(1)",
+        authors: [
+          "トミイ　大塚"
+        ],
+        imageLinks: {
+          smallThumbnail: "http://books.google.com/books/content?id=n0WU-RX8-yoC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+          thumbnail: "http://books.google.com/books/content?id=n0WU-RX8-yoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        }
       }
-     }
     }
-   ]
-  };
+  ]
+};
 
 const TEST_ENTRY_DATA = new BookEntry(_TEST_ENTRY_DATA);
